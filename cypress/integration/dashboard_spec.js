773294102915c2a55358f883918c07b30c0c2db4
Rachel Hendricks <rachelhen92@gmail.com>
2021-06-15 10:18:35 -0600

    Test title displays when visit page

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -3,13 +3,16 @@ describe('Home Page', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
       "urls": [{
         "id": 1,
-        "long_url": "https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2019/02/dog-451643.jpg?h=bf654dbc&itok=MQGvBmuo",
+        "long_url": "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
         "short_url": "http://localhost:3001/useshorturl/1",
-        "title": "Cute Dog"
+        "title": "Abstract Art"
       }]
     }
     )
     cy.visit('http://localhost:3000')
   })
 
+  it('should display the title', () => {
+    cy.contains('h1', 'URL Shortener')
+  })
 })
